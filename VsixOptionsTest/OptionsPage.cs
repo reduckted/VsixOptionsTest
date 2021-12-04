926a9feb641d5b62c58a76746fe9658cd353d69e
reduckted <reduckted@outlook.com>
2021-12-04 21:24:16 +1000

    Initial commit.

---
+++ VsixOptionsTest/OptionsPage.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using System.ComponentModel;
+
+namespace VsixOptionsTest {
+
+    public class OptionsPage : DialogPage {
+
+        [Category("Testing")]
+        [DisplayName("Enable something")]
+        [Description("A setting that enables something.")]
+        [DefaultValue(false)]
+        public bool IsEnabled { get; set; }
+
+
+        public override void SaveSettingsToStorage() {
+            base.SaveSettingsToStorage();
+        }
+
+        protected override void SaveSetting(PropertyDescriptor property) {
+            base.SaveSetting(property);
+        }
+
+        public override void SaveSettingsToXml(IVsSettingsWriter writer) {
+            base.SaveSettingsToXml(writer);
+        }
+
+        protected override void LoadSettingFromStorage(PropertyDescriptor prop) {
+            base.LoadSettingFromStorage(prop);
+        }
+
+        public override void LoadSettingsFromStorage() {
+            base.LoadSettingsFromStorage();
+        }
+
+        public override void LoadSettingsFromXml(IVsSettingsReader reader) {
+            base.LoadSettingsFromXml(reader);
+        }
+
+    }
+
+}
---
+++ VsixOptionsTest/VsixOptionsTestPackage.cs
@@ -0,0 +1,19 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace VsixOptionsTest {
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.VsixOptionsTestString)]
+    [ProvideOptionPage(typeof(OptionsPage), "Test Options", "General", 0, 0, true, ProvidesLocalizedCategoryName = false, SupportsProfiles = true)]
+    public sealed class VsixOptionsTestPackage : ToolkitPackage {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress) {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
